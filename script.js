da498c4b6cc116a3f8f00b1745249d845dbada39
syedwaheedulla <104793275+syedwaheedulla@users.noreply.github.com>
2024-02-23 22:50:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Example: Change the background color of the header when clicked
+document.querySelector('header').addEventListener('click', function() {
+    this.style.backgroundColor = '#555';
+});
